2b6ec0141ea2438004e0fd0f871cc7b347490412
tlsrltjq <117704577+tlsrltjq@users.noreply.github.com>
2024-05-22 16:39:17 +0900

    as

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, request
-from flaskext.mysql import MySQL
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'sin87531'
-app.config['MYSQL_DATABASE_DB'] = 'mydatabase'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'sin87531'
+app.config['MYSQL_DB'] = 'mydatabase'
+app.config['MYSQL_HOST'] = 'localhost'
 
-mysql = MySQL()
-mysql.init_app(app)
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -19,10 +19,17 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     name = request.form['name']
-    cursor = mysql.connect().cursor()
-    query = "SELECT * FROM users WHERE name LIKE %s"
-    cursor.execute(query, ('%' + name + '%',))
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    if name:
+        query = "SELECT * FROM users WHERE name LIKE %s"
+        cursor.execute(query, ('%' + name + '%',))
+    else:
+        query = "SELECT * FROM users"
+        cursor.execute(query)
+
     data = cursor.fetchall()
+    cursor.close()
     return render_template('results.html', data=data)
 
 if __name__ == "__main__":
