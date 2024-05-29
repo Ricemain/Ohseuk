524076e855fff99eab853884bb77fb8270857295
유녕유녕 <103240775+y293s@users.noreply.github.com>
2024-05-29 18:29:16 +0900

    세분화검색

---
+++ 세분화검색/script.js
@@ -0,0 +1,10 @@
+
+const detailButton = document.querySelector('.details');
+
+
+detailButton.addEventListener('click', function() {
+    
+    const nextPageUrl = 'file:///C:/Users/dbsdu/Desktop/cap/%EB%85%B8%EC%9D%B8%EB%A7%9E%EC%B6%A4%EB%8F%8C%EB%B4%84/old.html';
+   
+    window.location.href = nextPageUrl;
+});
