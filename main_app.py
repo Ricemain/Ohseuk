1d09f40cf6b17a282066c74f816d37351170cb2d
tlsrltjq <117704577+tlsrltjq@users.noreply.github.com>
2024-06-01 13:37:45 +0900

    검색 성공 ^_^

--- main_app.py
+++ main_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='메인페이지')
 # MySQL configurations
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'sin87531'
-app.config['MYSQL_DB'] = 'silverlink'
+app.config['MYSQL_DB'] = 'mydatabase'
 app.config['MYSQL_HOST'] = 'localhost'
 
 mysql = MySQL(app)
@@ -18,8 +18,7 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    data = request.get_json()
-    search_query = data.get('query')
+    search_query = request.form.get('query')
     
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
@@ -30,7 +29,6 @@ def search():
     results = cursor.fetchall()
     cursor.close()
 
-    # 검색 결과를 JSON 형식으로 반환합니다.
     return render_template('results.html', results=results)
 
 if __name__ == "__main__":
