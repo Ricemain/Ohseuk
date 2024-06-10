3cd44927c3314b39a4c05854d09744f343617748
유녕유녕 <103240775+y293s@users.noreply.github.com>
2024-06-10 18:53:57 +0900

    맞춤형복지안내

---
+++ 맞춤형복지안내/coustomized.js
@@ -0,0 +1,5 @@
+const potentialBenefitsCount = 10; 
+const currentBenefitsCount = 5; 
+
+document.getElementById('potentialBenefits').textContent = `${potentialBenefitsCount}건 가능`;
+document.getElementById('currentBenefits').textContent = `${currentBenefitsCount}건 이용중`;
