7aa0b1261992f1556d0d1ca0091ffd1adf7d8471
Ricemain <lakingsunk22@gmail.com>
2024-06-12 20:49:28 +0900

    login

---
+++ public/script/coustomized.js
@@ -0,0 +1,5 @@
+const potentialBenefitsCount = 0; 
+const currentBenefitsCount = 0; 
+
+document.getElementById('potentialBenefits').textContent = `${potentialBenefitsCount}건 가능`;
+document.getElementById('currentBenefits').textContent = `${currentBenefitsCount}건 이용중`;
